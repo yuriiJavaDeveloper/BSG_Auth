47b74bde7ca2d0520f7e323c471eab1d7d5aba99
Oliynuk Sviatoslav <sviatoslavov19102002@gmail.com>
2019-01-07 19:58:25 +0200

    Sviatoslav #1

--- app/src/main/java/com/example/xxxan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/xxxan/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.xxxan.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt1 = findViewById(R.id.button2);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+        Button bt2 = findViewById(R.id.button3);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+        Button bt3 = findViewById(R.id.button5);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main4Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
