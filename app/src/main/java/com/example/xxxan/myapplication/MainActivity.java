4e36a964e1d4c635a49b85d1091ff14543e831d6
Oliynuk Sviatoslav <sviatoslavov19102002@gmail.com>
2019-01-08 00:22:53 +0200

    Sviatoslav #2

--- app/src/main/java/com/example/xxxan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/xxxan/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.xxxan.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
