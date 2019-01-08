d85e7b27a196689bc91a9376993eed0049938c0b
Artem <greatwolf20xx@gmail.com>
2019-01-08 02:44:27 +0200

    create new activities

---
+++ app/src/main/java/com/example/xxxan/myapplication/Main7Activity.java
@@ -0,0 +1,29 @@
+package com.example.xxxan.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main7Activity extends AppCompatActivity {
+    public static TextView text;
+    Button buttonScan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textDesc);
+        buttonScan = findViewById(R.id.buttonQRcode);
+
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Main8Activity.class));
+            }
+        });
+    }
+}
+
